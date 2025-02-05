2cf4451e9e43dc9cf723450741875e943c759038
non247 <chhanon05@gmail.com>
2025-02-05 16:57:53 +0700

    Adidas

---
+++ lib/home_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('SHOP'),
+              backgroundColor: Colors.white,
+              actions: [
+                Container(
+                  margin: EdgeInsets.only(right: 16.0),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.person_2_outlined,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            body: Expanded(
+              child: Column(
+                children: [
+                //Search Bar
+                Container(
+                    width: double.infinity,
+                    height: 50,
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        top: BorderSide(
+                            color: Color.fromARGB(255, 240, 240, 240)),
+                        bottom: BorderSide(
+                            color: Color.fromARGB(255, 240, 240, 240)),
+                      ),
+                    ),
+                    child: Row(
+                      children: [const Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Icon(
+                        Icons.search,
+                        weight: 0.1,
+                      ),
+                    ),
+                    Text(
+                      'Find products...',
+                      style: GoogleFonts.lato(
+                        color: Colors.black26,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    ],
+                    )
+                    ),
+                    SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(left: 16.0),
+                        child: Text(
+                          'WOMEN',
+                          style: GoogleFonts.lato(
+                              color: Colors.black,
+                              fontSize: 12.0,
+                              letterSpacing: 2.0),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 16.0),
+                        child: Text(
+                          'MEN',
+                          style: GoogleFonts.lato(
+                              color: Colors.black,
+                              fontSize: 12.0,
+                              letterSpacing: 2.0),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 16.0),
+                        child: Text(
+                          'KIDS',
+                          style: GoogleFonts.lato(
+                              color: Colors.black,
+                              fontSize: 12.0,
+                              letterSpacing: 2.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ), // Category
+                SizedBox(
+                  width: double.infinity,
+                  height: 180,
+                  child: Image.asset(
+                    'assets/images/image-1.jpg',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+ 
+              ]),
+            )
+            )
+            );
+  }
+//bottomNavigator
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
